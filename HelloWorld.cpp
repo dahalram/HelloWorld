86b6bc592495881d1d05855534bcc70139a50f9f
unknown <ramhari.dahal.23@gmail.com>
2015-03-03 23:16:50 -0500

    Added Comments to the program.

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -3,7 +3,11 @@
 using namespace std;
 
 int main(){
+	//Print out the Hello World!
 	cout << "Hello World!" << endl;
 
+	//return statement because we used int main. Not important here but is required in other functions. 
+	//Good practice to use it.
+	
 	return 0;
 }
